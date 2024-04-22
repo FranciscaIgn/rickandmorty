889a1449625e8ef24137140e4a79cc4605d7c111
Francisca <franciscaigfv@gmail.com>
2024-04-22 16:42:42 -0400

    feat: create html, index.js get call API and character,created cards

---
+++ ASSETS/js/index.js
@@ -0,0 +1,54 @@
+const URL_BASE = "https://rickandmortyapi.com/api/"
+
+// 1 Traer los datos de la API
+
+const getallcharacter = async() => {
+    try{
+        const response = await fetch(`${URL_BASE}character`)
+        const data = await response.json()
+
+        
+        return data
+   
+    }catch(error){
+        console.log('no me salio')
+    }
+}
+
+getallcharacter()
+
+//  2 crear tarjeta para los personajes
+
+const createcharactercard = async() => {
+    try{
+        const getcharacterdata = await getallcharacter()
+        console.log(getcharacterdata)
+        const characterdata = getcharacterdata.results
+        console.log(characterdata)
+
+        let htmlcode = '';
+        characterdata.forEach((character) => {
+            htmlcode +=`
+            <div class="character-card">
+              <div class="character-img">
+                  <img src="${character.image}" alt="${character.name} image">
+              </div>
+              <div class="character-card-body">
+                    <h2 class="character-title">${character.name}</h2>
+                    <h3 class="character-subtitle">Species</h3>
+                    <p class="character-species-status">${character.species} - ${character.status}</p>
+                    <button class="btn-character-card">Click for more Info</button>
+              </div>
+            </div>
+            `
+            return htmlcode // return del foreach
+        })
+            console.log(htmlcode)
+            return htmlcode // return de mi funcion grande
+    }catch(error){
+        console.log('no me salio :C')
+    }
+}
+createcharactercard()
+
+// 3 imprimir las tarjetas
